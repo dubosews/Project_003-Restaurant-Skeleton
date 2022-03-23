88fcb514a140d1588b81a2dfd60e08f53e299d1d
rclank <53981655+rclank@users.noreply.github.com>
2022-03-23 19:35:06 -0400

    cleanup front-end

---
+++ p03-restaurant-skeleton/client/src/components/RecipeForm/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+
+import { ADD_RECIPE } from '../../utils/mutations';
+import { QUERY_RECIPES } from '../../utils/queries';
+
+const RecipetForm = () => {
+
+};
+
+export default RecipeForm;
--- p03-restaurant-skeleton/client/src/components/pages/Home.js
+++ p03-restaurant-skeleton/client/src/components/pages/Home.js
@@ -1,13 +1,5 @@
 import React from 'react';
 
 export default function Home() {
-  function buttonT() {
-    console.log('hellobtn')
-  }
 
-  return (
-  
-    <button type="button" className="btn btn-outline-primary" onClick={() => buttonT()}>Primary</button>
-   
-  );
 }
